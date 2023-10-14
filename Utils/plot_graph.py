0650b8d4a404d8d85d46d99a7f47e5c89202e019
Zeinab-Mohsen <zeinabmohsen41@gmail.com>
2023-10-14 21:06:01 +0300

    GUI

--- Utils/plot_graph.py
+++ Utils/plot_graph.py
@@ -15,21 +15,24 @@ def plot_data(
     y_label="",
     fig_size=[20, 4],
     sub_plot=[1, 2, 1],
+    ax=None,
+    canvas=None,
 ):
+    ax.clear()
     # Set plot parameters
-    plt.figure(figsize=(fig_size[0], fig_size[1]))
-    plt.subplot(sub_plot[0], sub_plot[1], sub_plot[2])
+    # ax.figure.set_size_inches(fig_size[0], fig_size[1])
+    # plt.subplot(sub_plot[0], sub_plot[1], sub_plot[2])
 
     # Create plot based on plot type
     if plot_type == "continuous":
-        plt.plot(x, y)
+        ax.plot(x, y)
     elif plot_type == "discrete":
-        plt.scatter(x, y)
-        plt.stem(x, y)
+        ax.scatter(x, y)
+        ax.stem(x, y)
 
     # Add labels and title
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
 
-    plt.show()
+    canvas.draw()
