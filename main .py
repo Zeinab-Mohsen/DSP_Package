c0fbd9a14ea5fa51ebbec1de60fcd8873f8f103f
Zeinab-Mohsen <zeinabmohsen41@gmail.com>
2023-10-23 17:37:31 +0300

    delete

--- main .py
+++
@@ -1,8 +0,0 @@
-from root import root
-from GUI.Utils.main_frames_manipulation import show_read_signal_file_frame, show_addition_frame
-import GUI.menu
-
-# show_read_signal_file_frame()
-show_addition_frame()
-
-root.mainloop()
