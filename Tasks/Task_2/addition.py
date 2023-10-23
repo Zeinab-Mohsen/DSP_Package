8dc633d0d54f33bf23695885c3d70ca8adfd4719
Zeinab-Mohsen <zeinabmohsen41@gmail.com>
2023-10-23 21:48:39 +0300

    addition

--- Tasks/Task_2/addition.py
+++ Tasks/Task_2/addition.py
@@ -6,6 +6,7 @@ from Utils.plot_graph import plot_data
 from Utils.read_signal_file import seperate_file_data
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from Test.Sin_Cos.comparesignals import SignalSamplesAreEqual
 import sys
 
 sys.path.append("../")
@@ -14,6 +15,7 @@ addition_frame = tk.Frame(root)
 
 addition_label = CustomLabel(addition_frame, text="Addition")
 addition_label.pack()
+
 
 def browse_button_command(title, ax, canvas):
     data, signal_type, is_periodic, n_samples = seperate_file_data()
@@ -30,31 +32,50 @@ def browse_button_command(title, ax, canvas):
         ax=ax,
         canvas=canvas,
     )
-    return y
-
-y1 = []
-y2 = []
-def load_signal1():
-    global y1
-    y1 = browse_button_command("First Signal", ax_signal1, canvas_signal1)
-
-def load_signal2():
-    global y2
-    y2 = browse_button_command("Second Signal", ax_signal2, canvas_signal2)
+    return data
 
 
-addition_buttons_frame = tk.Frame(addition_frame)
-addition_buttons_frame.pack()
-first_signal_button = CustomButton(
-    addition_buttons_frame, command=load_signal1, text="Read first signal"
+def add_signals():
+    data = browse_button_command("First Signal", ax_signal1, canvas_signal1)
+    x1 = [point[0] for point in data]
+    y1 = [point[1] for point in data]
+
+    data = browse_button_command("Second Signal", ax_signal2, canvas_signal2)
+    x2 = [point[0] for point in data]
+    y2 = [point[1] for point in data]
+
+    max_len = max(len(y1), len(y2))
+
+    # Pad the shorter list with zeros to match the length of the longer list
+    y1 = y1 + [0] * (max_len - len(y1))
+    y2 = y2 + [0] * (max_len - len(y2))
+
+    # Add the two lists together element-wise
+    result = [x + y for x, y in zip(y1, y2)]
+
+    print("Addition of signal 1 + signal 2")
+    SignalSamplesAreEqual("Output\Task_2\Signal1+signal2.txt", 0, result)
+
+    print("Addition of signal 1 + signal 3")
+    SignalSamplesAreEqual("Output\Task_2\signal1+signal3.txt", 0, result)
+
+
+    plot_data(
+        x=x1,
+        y=result,
+        plot_type="continuous",
+        title="Signal 1 + Signal 2",
+        x_label="Time",
+        y_label="Amplitude",
+        ax=ax_resultant,
+        canvas=canvas_resultant,
+    )
+
+
+add_signals_button = CustomButton(
+    addition_frame, command=add_signals, text="Add two signals"
 )
-first_signal_button.pack(side=tk.LEFT, padx=250, pady=10)
-
-second_signal_button = CustomButton(
-    addition_buttons_frame, command=load_signal2, text="Read second signal"
-)
-second_signal_button.pack(side=tk.LEFT, padx=250, pady=10)
-
+add_signals_button.pack(side=tk.TOP, padx=250, pady=10)
 
 # Add signal plot frame
 signals_representation_frame = tk.Frame(addition_frame)
@@ -62,15 +83,11 @@ signals_representation_frame.pack(padx=15, pady=5)
 
 # Add continuous signal representation frame
 signal1_frame = tk.Frame(signals_representation_frame)
-signal1_frame.pack(
-    padx=30, pady=2, side=tk.LEFT, expand=True, fill=tk.BOTH
-)
+signal1_frame.pack(padx=30, pady=2, side=tk.LEFT, expand=True, fill=tk.BOTH)
 
 # Add discrete signal representation frame
 signal2_frame = tk.Frame(signals_representation_frame)
-signal2_frame.pack(
-    padx=30, pady=2, side=tk.LEFT, expand=True, fill=tk.BOTH
-)
+signal2_frame.pack(padx=30, pady=2, side=tk.LEFT, expand=True, fill=tk.BOTH)
 
 # Add empty figure for continuous signal representation
 fig, ax_signal1 = plt.subplots()
@@ -90,12 +107,5 @@ resultant_signal_representation_frame.pack(padx=2, pady=10, expand=True, fill=tk
 # Add empty figure for continuous signal representation
 fig, ax_resultant = plt.subplots()
 fig.set_size_inches(20, 5)
-canvas_resultant = FigureCanvasTkAgg(
-    fig, resultant_signal_representation_frame
-)
+canvas_resultant = FigureCanvasTkAgg(fig, resultant_signal_representation_frame)
 canvas_resultant.get_tk_widget().pack(fill=tk.BOTH, expand=True)
-
-
-
-print(y1)
-print(y2)
